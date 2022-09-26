98dd05a269db9ade3c0ed704bdc7b28bb74fd5a0
r5zYoon <asaucybloom@gmail.com>
2022-09-26 12:15:10 +0900

    second commit

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def dinner(request, name):
+    menus = [{"name":'poke',"price":10000}, {"name":'bibimbob',"price":9000}, {"name":'gimbob',"price":5000}, {"name":'sushi',"price":15000}]
+    pick = random.choice(menus)
+    context = {
+        'pick': pick,
+        'name': name,
+        'menus': menus,
+    }
+
+    return render(request, 'dinner.html', context)
