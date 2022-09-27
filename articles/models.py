a7fc95a0f8c92110db42a4889051fcaf01cbbe52
r5zYoon <asaucybloom@gmail.com>
2022-09-27 21:08:31 +0900

    Add DB, admin

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model): #상속
+    #id는 자동 생성됨. 아래 세 가지가 가장 많이 쓰이는 모델필드이다.
+    title = models.CharField(max_length=10)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
